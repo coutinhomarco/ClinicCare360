a6b64dabf9c33481585707681d7765d87866a95d
coutinhomarco <marquinco@outlook.com>
2024-05-11 20:52:48 -0300

    forgot to add this bit of code :/

--- server/src/api/services/patient/PatientCommandService.ts
+++ server/src/api/services/patient/PatientCommandService.ts
@@ -1,18 +1,15 @@
 import { PatientModel } from '../../models/PatientModel';
 import { UserModel } from '../../models/UserModel';
-import { PatientData, isValidPatientData, isValidPatientDelete } from '../../utils/validations/patientValidation';
+import { PatientData, isValidPatientData, isValidPatientDelete, isValidPatientUpdateData } from '../../utils/validations/patientValidation';
 import { ServiceResponse } from '../../../@types/ServiceResponse';
 
 
 export class PatientCommandService {
     static async createPatient(patientData: any): Promise<ServiceResponse<any>> {
         try {
-            const user = await UserModel.findUserByEmail(patientData.email);
-            if (!user) {
-                return { status: 404, message: 'User not found' };
-            }
-            if (!isValidPatientData(patientData)) {
-                return { status: 400, message: 'Invalid patient data' };
+            const { status, message } = await isValidPatientData(patientData);
+            if (status !== 200) {
+                return { status, message };
             }
             await PatientModel.create(patientData);
             return { status: 201, message: 'Patient created successfully'};
@@ -23,16 +20,7 @@ export class PatientCommandService {
 
     static async updatePatient(id: number, patientData: Partial<PatientData>): Promise<ServiceResponse<any>> {
         try {
-            const user = await UserModel.getUserById(id);
-            if (!user) {
-                return { status: 404, message: 'User not found' };
-            }
-            const fieldsToUpdate = Object.keys(patientData) as (keyof PatientData)[];
-            for (const key of fieldsToUpdate) {
-                if (patientData[key] === undefined) {
-                    return { status: 400, message: `Invalid data for field: ${key}` };
-                }
-            }
+            const { status, message } = await isValidPatientUpdateData(patientData);
             const updatedPatient = await PatientModel.update(id, patientData);
             return { status: 200, data: updatedPatient };
         } catch (error: any) {

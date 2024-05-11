a6382c8d9b764605a66b9876231432c0deb90831
coutinhomarco <marquinco@outlook.com>
2024-05-11 12:31:07 -0300

    improve validations

--- server/src/api/services/appointment/AppointmentCommandService.ts
+++ server/src/api/services/appointment/AppointmentCommandService.ts
@@ -1,4 +1,10 @@
 import { AppointmentModel } from '../../models/AppointmentModel';
+interface ServiceResponse<T> {
+    status: number;
+    data?: T;
+    message?: string;
+}
+
 interface AppointmentData {
     patientId: number;
     doctorId: number;
@@ -7,31 +13,32 @@ interface AppointmentData {
     endTime: Date;
     status: string;
 }
-interface ServiceResponse<T> {
-    status: number;
-    data?: T;
-    message?: string;
-}
 
 function isValidAppointmentData(data: any): data is AppointmentData {
-    return 'patientId' in data && 'doctorId' in data && 'appointmentDate' in data &&
-           'startTime' in data && 'endTime' in data && 'status' in data;
+    return typeof data.patientId === 'number' &&
+           typeof data.doctorId === 'number' &&
+           data.appointmentDate instanceof Date &&
+           data.startTime instanceof Date &&
+           data.endTime instanceof Date &&
+           typeof data.status === 'string';
 }
-
-
 export class AppointmentCommandService {
     static async createAppointment(appointmentData: any): Promise<ServiceResponse<any>> {
         if (!isValidAppointmentData(appointmentData)) {
-            return { status: 400, message: 'Invalid appointment data' };
+            return { status: 400, message: 'Invalid appointment data. Please ensure all fields are correctly formatted and provided.' };
         }
         const newAppointment = await AppointmentModel.create(appointmentData);
         return { status: 201, data: newAppointment };
     }
 
-    static async updateAppointment(id: number, appointmentData: any): Promise<ServiceResponse<any>> {
-        if (Object.keys(appointmentData).length === 0) {
-            return { status: 400, message: 'No data provided for update' };
+    static async updateAppointment(id: number, appointmentData: Partial<AppointmentData>): Promise<ServiceResponse<any>> {
+        const fieldsToUpdate = Object.keys(appointmentData) as (keyof AppointmentData)[];
+        for (const key of fieldsToUpdate) {
+            if (appointmentData[key] === undefined) {
+                return { status: 400, message: `Invalid data for field: ${key}` };
+            }
         }
+        
         const updatedAppointment = await AppointmentModel.update(id, appointmentData);
         return { status: 200, data: updatedAppointment };
     }
@@ -41,4 +48,3 @@ export class AppointmentCommandService {
         return { status: 204 };
     }
 }
-

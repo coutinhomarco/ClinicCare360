8534fdb9b174beff038fdc4f2fde3b74ccba3b24
coutinhomarco <marquinco@outlook.com>
2024-05-13 16:48:11 -0300

    update appointmentcontroller

--- server/src/api/controllers/appointment/AppointmentCommandController.ts
+++ server/src/api/controllers/appointment/AppointmentCommandController.ts
@@ -1,36 +1,23 @@
 import { Request, Response } from 'express';
 import { AppointmentCommandService } from '../../services/appointment/AppointmentCommandService';
-import { isValidAppointmentDelete, isValidAppointmentData, isValidAppointmentUpdateData } from '../../utils/validations/appointmentValidation';
 
 export class AppointmentCommandController {
     static async createAppointment(req: Request, res: Response) {
         const appointmentData = req.body;
-        const { status, message } = await isValidAppointmentData(appointmentData.patientId);
-        if (status !== 200) {
-            return res.status(status).json({ message });
-        }
-        const {status: statusFinal, message: messageFinal} = await AppointmentCommandService.createAppointment(appointmentData);
-        return res.status(statusFinal).json({message: messageFinal});
+        const result = await AppointmentCommandService.createAppointment(appointmentData);
+        return res.status(result.status).json({ message: result.message });
     }
 
     static async updateAppointment(req: Request, res: Response) {
         const id = parseInt(req.params.id);
         const appointmentData = req.body;
-        const { status, message } = await isValidAppointmentUpdateData(appointmentData.patientId);
-        if (status !== 200) {
-            return res.status(status).json({ message });
-        }
-        const {status: statusFinal, message: messageFinal} = await AppointmentCommandService.updateAppointment(id, appointmentData);
-        return res.status(statusFinal).json({message: messageFinal});
+        const result = await AppointmentCommandService.updateAppointment(id, appointmentData);
+        return res.status(result.status).json({ message: result.message });
     }
 
     static async deleteAppointment(req: Request, res: Response) {
-        const { id } = req.params;
-        const { status, message } = await isValidAppointmentDelete(Number(id));
-        if (status !== 200) {
-            return res.status(status).json({ message });
-        }
-        const {status: statusFinal, message: messageFinal} =await AppointmentCommandService.deleteAppointment(Number(id));
-        return res.status(statusFinal).json({ message: messageFinal});
+        const id = parseInt(req.params.id);
+        const result = await AppointmentCommandService.deleteAppointment(id);
+        return res.status(result.status).json({ message: result.message });
     }
 }
